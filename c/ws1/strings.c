e17ec4f73400b6db6201318eb07322a1473a602d
itay.lvovich <itaylvovich@gmail.com>
2024-04-08 10:35:00 +0300

    updated strings.c, strings.h exercises

--- c/ws1/strings.c
+++ c/ws1/strings.c
@@ -1,23 +1,29 @@
-#include <stdio.h>
-#include <assert.h>
-#include "strfuncs.h"
+#include <stdio.h>	/* printf */
+#include <stdlib.h>	/* malloc */
+#include <assert.h>	/* assert */
+#include <string.h>	/*strlen in main() for testing purposes, tolower */
+#include "strings.h"	/* all implementations of string.h exercises */
+#include <ctype.h>	/* tolower */
 
 int main()
 {
 
-	char *st="abcde";
-	printf("%zu\n\n", StrLen(st));
+	char st[] = "abEs";
+	char st2[] = "abe";
+	printf("%zu \n\n", strlen(st));
+	printf("%zu \n\n", sizeof(st));
+	char *dest = (char *)malloc(strlen(st) +1 );
 	
-	char *str2 = "abcee";
+	if (dest == NULL)
+		return 1;
 	
-	printf("%d\n\n", StrCmp(st, str2));
+	char *ch = StrChr(st, 's');
 	
-	int num = 4;
+	printf("%c \n", *ch);
 	
-	num = 2 * num++;
+	free(dest);
 	
-	
-	printf("%d \n", num);
+	return 0;
 }
 
 
@@ -46,3 +52,87 @@ int StrCmp(const char *str1, const char *str2)
 	}
 	return ((*str1) - (*str2));
 }
+
+
+char* StrCpy(char* destination, const char* source)
+{
+	assert(destination);
+	assert(source);
+	int i = 0;
+	while (source[i] != '\0')
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = source[i];
+	return destination;
+}
+
+char *StrnCpy(char *dest, const char *src, size_t n)
+{
+	assert(dest);
+	assert(src);
+	int i = 0;
+	while (src[i] != '\0'&& i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	
+	
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	
+	}
+	dest[i] = '\0';
+	
+	return dest;
+}
+
+
+int StrnCmp(const char *str1, const char *str2, size_t n)
+{
+	assert(str1);
+	assert(str2);
+	int count = 1;
+	while (*str1 == *str2 && *str1 != '\0' && *str2 != '\0' && count < n)
+	{
+		str1++;
+		str2++;
+		count++;
+	}
+	return ((*str1) - (*str2));
+}
+
+
+int StrCaseCmp(const char *s1, const char *s2)
+{
+ 	assert(s1);
+	assert(s2);
+	while (tolower(*s1) == tolower(*s2) && *s1 != '\0' && *s2 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	return ((*s1) - (*s2));
+ }
+ 
+ 
+char *StrChr(const char *str, int c)
+{
+	const char *ptr = str;
+	while (*ptr != '\0')
+	{
+		if (*ptr == c)
+		{
+			return ptr;
+		}
+		ptr++;
+	}
+	return NULL;
+}
+
+
+
--- c/ws1/strings.h
+++ c/ws1/strings.h
@@ -5,4 +5,14 @@ size_t StrLen(const char *str);
 
 int StrCmp(const char *str1, const char *str2);
 
+char *StrCpy(char* destination, const char* source);
+
+char *StrnCpy(char *dest, const char *src, size_t n);
+
+int StrnCmp(const char *str1, const char *str2, size_t n);
+
+ int StrCaseCmp(const char *s1, const char *s2);
+ 
+ char *StrChr(const char *str, int c);
+
 #endif
