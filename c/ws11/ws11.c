63c38a129d587f89522560c07e9a4b2ec582ab66
itay.lvovich <itaylvovich@gmail.com>
2024-04-30 15:33:27 +0300

    implemented itoa + atoi

--- c/ws11/ws11.c
+++ c/ws11/ws11.c
@@ -9,7 +9,31 @@ static char *ReverseStringHelper(char *str);
 int Atoi(const char *str)
 {
 	int result = 0;
-	return 0;
+	char negative_flag = *str;
+	const char *runner = str;
+	
+	if (negative_flag == '-')
+	{
+		++runner;
+	}
+	
+	while (*runner != '\0')
+	{
+		if (*runner < '0' || *runner > '9')
+		{
+			return 0;
+		}
+		result = result * 10;
+		result = result + (*runner - '0');
+		++runner;
+	}
+	
+	if (negative_flag == '-')
+	{
+		result = result * -1;
+	}
+	
+	return result;
 }
 
 
@@ -18,24 +42,33 @@ char *Itoa(int num, char* buffer, int base)
 	int remainder = 0;
 	char *runner = buffer;
 	char tmp_ch = '\0';
+	int negative_flag_buffer = 0;
 	
 	if (num < 0)
 	{
-		*runner = '-';
-		++runner;
+		if (10 == base)
+		{
+			*runner = '-';
+			num = num * -1;
+			++runner;
+			negative_flag_buffer = 1;
+		}
+		else
+		{
+			return NULL;
+		}
 	}
 	
 	while (num > 0)
 	{
 		remainder = num % base;
-		
 		if (remainder <= 9)
 		{
 			tmp_ch = (char)(remainder + '0');
 		}
 		else
 		{
-			tmp_ch = (char)((remainder - 10) + 'A');
+			tmp_ch = (char)((remainder - 10) + 'a');
 		}
 		*runner = tmp_ch;
 		++runner;
@@ -43,7 +76,12 @@ char *Itoa(int num, char* buffer, int base)
 	}
 	*runner = '\0';
 	
-	buffer = ReverseStringHelper(buffer);
+	buffer = ReverseStringHelper(buffer + negative_flag_buffer);
+	
+	if (negative_flag_buffer == 1)
+	{
+		--buffer;
+	}
 	
 	return buffer;
 }
--- c/ws11/ws11_test.c
+++ c/ws11/ws11_test.c
@@ -1,22 +1,47 @@
-#include <stdio.h>	/*printf*/
-
+#include <stdio.h>	/* printf */
+#include <stdlib.h>	/* atoi, itoa */
+#include <assert.h>	/* assert */
 #include "ws11.h"
 
+void TestAtoi(char *str);
+void TestItoa();
 
 int main()
 {
 	char str[100];
+	puts("enter a string that represents a number: \n");
+	scanf("%s", str);
+	TestAtoi(str);
+	TestAtoi("0");
+	TestAtoi("12345678");
+	TestAtoi("-12345678");
+	TestAtoi("4294967295"); /* 2^32 */
+	TestAtoi("abc123abc");
 	
-	printf("Number: %d\nBase: %d\tConverted String: %s\n", 1567, 10, Itoa(1567, str, 10));
+	TestItoa();
 	
-	printf("Base: %d\t\tConverted String: %s\n", 2, Itoa(1567, str, 2));
-	
-	printf("Base: %d\t\tConverted String: %s\n", 8, Itoa(1567, str, 8));
-	
-	printf("Base: %d\tConverted String: %s\n", 16, Itoa(1567, str, 16));
-	
-	
+
 	return 0;
 }
 
+void TestAtoi(char *str)
+{
+	printf("\natoi: %d\nAtoi: %d\n", atoi(str), Atoi(str));
+	assert(atoi(str) == Atoi(str));
+	printf("assert Atoi equal to atoi for string = '%s' passed\n", str);
+}
 
+void TestItoa()
+{
+	char str[100];
+
+	printf("\nItoa test: \n");
+	
+	printf("number: %d\nbase: %d\tconverted String: %s\n", 1234, 10, Itoa(1234, str, 10));
+	
+	printf("base: %d\t\tConverted String: %s\n", 2, Itoa(1234, str, 2));
+	
+	printf("base: %d\t\tConverted String: %s\n", 8, Itoa(1234, str, 8));
+	
+	printf("base: %d\tConverted String: %s\n", 16, Itoa(1234, str, 16));
+}
