6bdbf743376c0d74d0f39856337ffac9ac578e2e
itay.lvovich <itaylvovich@gmail.com>
2024-05-02 18:15:21 +0300

    working version of all functions of stack, added tests for all functions

--- ds/test/stack_test.c
+++ ds/test/stack_test.c
@@ -11,20 +11,31 @@ int main()
 {
 	
 	stack_t *stack_ptr;
-	int x = 5;
+	int x = 1;
 	int *xp = (int *)malloc(sizeof(int) * 1);
 	int *test_ptr = (int *)malloc(sizeof(int) * 1);
 	xp = &x;
 	
 	
 	stack_ptr = StackCreate(50, sizeof(int));
-	printf("%ld, %ld\n", StackSize(stack_ptr), StackCapacity(stack_ptr));
-	
+	printf("stack size = %ld, stack capacity = %ld\n", StackSize(stack_ptr), StackCapacity(stack_ptr));
+	printf("pushed 1\n");
 	StackPush(stack_ptr, xp);
-	printf("%ld, %ld\n", StackSize(stack_ptr), StackCapacity(stack_ptr));
+	printf("stack size = %ld, stack capacity = %ld\n", StackSize(stack_ptr), StackCapacity(stack_ptr));
 	
+	x = 2;
+	StackPush(stack_ptr, xp);
+	printf("pushed 2\n");
+	x = 3;
+	StackPush(stack_ptr, xp);
+	printf("pushed 3\n");
 	StackPeek(stack_ptr, test_ptr);
-	printf("%d\n", *test_ptr);
+	printf("peek: %d\n", *test_ptr);
+	StackPop(stack_ptr);
+	printf("popped\n");
+	StackPeek(stack_ptr, test_ptr);
+	printf("peek: %d\n", *test_ptr);
+	printf("stack size = %ld, stack capacity = %ld\n", StackSize(stack_ptr), StackCapacity(stack_ptr));
 	
 	return 0;
 }
