83db73bca82cefd72fea488134254a4ff085129e
itay.lvovich <itaylvovich@gmail.com>
2024-05-06 10:20:36 +0300

    fixed test logic in main()

--- ds/test/bitarray_test.c
+++ ds/test/bitarray_test.c
@@ -18,13 +18,13 @@ int main()
 {
 	int program_exit_status = 0;
 	
-	program_exit_status = TestSetAll();
-	program_exit_status = TestResetAllAndCount();
-	program_exit_status = TestSetOffAndOn();
-	program_exit_status = TestGetVal();
-	program_exit_status = TestFlip();
-	program_exit_status = TestRotate();
-	program_exit_status = TestMirror();
+	program_exit_status += TestSetAll();
+	program_exit_status += TestResetAllAndCount();
+	program_exit_status += TestSetOffAndOn();
+	program_exit_status += TestGetVal();
+	program_exit_status += TestFlip();
+	program_exit_status += TestRotate();
+	program_exit_status += TestMirror();
 	
 	return program_exit_status;
 }
