73b303d11bdc56deb9d1ca65c4f9c01687dc2d9c
itay.lvovich <itaylvovich@gmail.com>
2024-05-07 08:34:54 +0300

    implemented AccessVal and tests for it for vector.c

--- ds/test/vector_test.c
+++ ds/test/vector_test.c
@@ -9,15 +9,26 @@ int main()
 	vector_t *vector;
 	int x = 5;
 	int i = 0;
+	int *int_ptr = NULL;
+	int *int_ptr2 = NULL;
 	vector = VectorCreate(2,  sizeof(int));
 	
 	for (i = 0; i < 10; ++i)
 	{
 		printf("element count = %ld\n", VectorElementCount(vector));	
 		printf("capacity = %ld\n", VectorCapacity(vector));
+		
 		VectorPushBack(vector, &x);
+		
+		int_ptr = VectorAccessVal(vector, i);
+		printf("accessval = %d\n", *int_ptr);
+		
+		*int_ptr = 999;
+		*(int *)VectorAccessVal(vector, i) = *int_ptr;
+		
+		int_ptr2 = VectorAccessVal(vector, i);
+		printf("accessval = %d\n", *int_ptr2);
 	}
-	
 	printf("\n");
 	printf("shrink:\n");
 	VectorShrink(vector);
@@ -33,16 +44,16 @@ int main()
 	printf("element count = %ld\n", VectorElementCount(vector));	
 	printf("capacity = %ld\n", VectorCapacity(vector));
 	printf("\n");
-	/*
+	
 	printf("shrink:\n");
-	VectorShrink(vector); */
+	VectorShrink(vector);
 	printf("element count = %ld\n", VectorElementCount(vector));	
 	printf("capacity = %ld\n", VectorCapacity(vector));
 	printf("\n");
 	
 	for (i = 0; i < 15; ++i)
 	{
-		printf("element count = %ld\n", VectorElementCount(vector));	
+		printf("element count = %ld\n", VectorElementCount(vector));
 		printf("capacity = %ld\n", VectorCapacity(vector));
 		VectorPopBack(vector);
 	}
