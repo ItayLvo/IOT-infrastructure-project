b02947021155da668791d14fde43d11523e6bad9
itay.lvovich <itaylvovich@gmail.com>
2024-05-28 17:49:41 +0300

    added tests for UID data structure

--- ds/test/uid_test.c
+++ ds/test/uid_test.c
@@ -10,11 +10,14 @@ int main()
 	
 	printf("%ld\n", uid1.timestamp);
 	printf("%ld\n", uid1.counter);
-	printf("%d\n", uid1.pid);
+	printf("%d\n\n", uid1.pid);
 	
 	printf("%ld\n", uid2.timestamp);
 	printf("%ld\n", uid2.counter);
-	printf("%d\n", uid2.pid);
+	printf("%d\n\n", uid2.pid);
+	
+	printf("testing IsEqual with GetBad (expected result is 0): %d\n", UIDIsEqual((uid1), UIDGetBad()));
+	printf("testing IsEqual with two UID_t (expected result is 0) %d\n:", UIDIsEqual(uid1, uid2));
 	
 	return 0;
 }
