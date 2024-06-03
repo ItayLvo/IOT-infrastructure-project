5beb514565118a96df38aaa00b494eb952771c96
itay.lvovich <itaylvovich@gmail.com>
2024-06-03 12:43:38 +0300

    fixed size allocator: initial working version ready, basic tests

--- system programming/src/fixed_size_allocator.c
+++ system programming/src/fixed_size_allocator.c
@@ -5,48 +5,108 @@
 
 #include "fixed_size_allocator.h"		/* FSA functions, typedefs */
 
+#define WORD_SIZE 8
 
 struct fixed_size_allocator
 {
-	allocator_block_t *free_list_head;
-	size_t block_size;
-	size_t current_free_blocks;
-	size_t pool_size; 
 	void *free_list;
 };
 
-typedef struct 
-{
-    void *next;
-} allocator_block_t;
+static size_t AlignBlockSize(size_t block_size);
+
 
 fixed_size_allocator_t *FSAInitialize(void *memory_pool, size_t block_size, size_t pool_size)
 {
-	fixed_size_allocator_t *allocator = ( fixed_size_allocator_t *)memory_pool;
+	fixed_size_allocator_t *allocator = (fixed_size_allocator_t *)memory_pool;
 	size_t index = 0;
-	char *block = NULL;
+	char *start_of_blocks = NULL;
+	char *current_block = NULL;
+	size_t total_blocks = 0;
+	
 	assert(memory_pool);
 	
-	allocator->block_size = block_size;
-	allocator->pool_size = pool_size;
-	allocator->current_free_blocks = (pool_size - sizeof(fixed_size_allocator_t)) / block_size;
-	allocator->free_list = (char *)allocator + sizeof(fixed_size_allocator_t);
+	block_size = AlignBlockSize(block_size);
 	
-	block = allocator->free_list;
-	for (index = 0; index < allocator->current_free_blocks; ++index)
+	total_blocks = (pool_size - sizeof(fixed_size_allocator_t)) / block_size;
+	
+	start_of_blocks = (char *)allocator + sizeof(fixed_size_allocator_t);
+	allocator->free_list = (void *)start_of_blocks;
+	
+	current_block = start_of_blocks;
+	for (index = 0; index < total_blocks - 1; ++index)
 	{
-		*((void **)block) = block + block_size;		/******* not sure at all... ********/
-		block += block_size;
+		*(void **)current_block = current_block + block_size;
+		current_block += block_size;
 	}
-	*((void **)block) = NULL;
+	*(void **)current_block = NULL;
 	
 	
 	return allocator;
 }
 
 
+void *FSAAlloc(fixed_size_allocator_t *allocator)
+{
+	void *allocated_block = NULL;
+	
+	assert(allocator);
+	if (allocator->free_list == NULL)
+	{
+		return NULL;
+	}
+	
+	allocated_block = allocator->free_list;
+	allocator->free_list = *(void **)allocator->free_list;
+
+	return allocated_block;
+}
+
+void FSAFree(fixed_size_allocator_t *allocator, void *mem_to_free_ptr)
+{
+	*(void **)mem_to_free_ptr = allocator->free_list;
+	
+	allocator->free_list = mem_to_free_ptr;
+}
+
+
+size_t FSACountFree(fixed_size_allocator_t *allocator)
+{
+	void *runner = allocator->free_list;
+	size_t count = 0;
+	
+	while (NULL != runner)
+	{
+		runner = *(void **)runner;
+		++count;
+	}
+	
+	return count;
+}
 
 size_t FSASuggestSize(size_t block_size, size_t block_count)
 {
+	block_size = AlignBlockSize(block_size);
+	
 	return (sizeof(fixed_size_allocator_t) + (block_count * block_size));
+}
+
+
+
+
+static size_t AlignBlockSize(size_t block_size)
+{
+	if (block_size < sizeof(void *))
+	{
+			block_size = sizeof(void *);
+			return block_size;
+	}
+	
+	if (block_size % WORD_SIZE == 0)
+	{
+		return block_size;
+	}
+	
+	block_size = (((block_size / WORD_SIZE) + 1) * WORD_SIZE);
+	
+	return block_size;
 }
--- system programming/test/fixed_size_allocator_test.c
+++ system programming/test/fixed_size_allocator_test.c
@@ -1,9 +1,30 @@
 #include <stdio.h>	/* printf */
-#include "fixed_size_allocator.h"
+#include <stdlib.h>	/* malloc */
+
+#include "fixed_size_allocator.h"	/* fixed_size_allocator_t and FSA functions */
 
 int main()
 {
-
+	size_t block_size = 8, block_count = 10;
+	size_t suggested_size = FSASuggestSize(block_size, block_count);
+	int *pool = malloc(suggested_size);
+	int *test_ptr_1 = NULL;
+	
+	fixed_size_allocator_t * allocator = FSAInitialize(pool, block_size, suggested_size);
+	
+	printf("suggested = %ld\n", suggested_size);
+	printf("countfree = %ld\n", FSACountFree(allocator));
+	
+	test_ptr_1 = FSAAlloc(allocator);
+	*test_ptr_1 = 5;
+	printf("%d\n", *test_ptr_1);
+	printf("countfree = %ld\n", FSACountFree(allocator));
+	
+	FSAFree(allocator, test_ptr_1);
+	printf("countfree = %ld\n", FSACountFree(allocator));
+	
+	free(pool);
+	
 	return 0;
 }
 
