4778568ae0d053661e34ed748dfc07a7faeb9509
itay.lvovich <itaylvovich@gmail.com>
2024-06-15 17:36:58 +0300

    implemented and tested bubble, insertion, selection sorts and becnhmarked vs qsort

--- ds/src/comparison_sorts.c
+++ ds/src/comparison_sorts.c
@@ -1,28 +1,68 @@
 #include <stdio.h>	/* printf */
+#include <stdlib.h>	/* rand */
+#include <time.h>	/* time() */
+
+#define SIZE 5000
 
 void InsertionSort(int *arr, int n);
-void BubbleSort(int arr[], int n);
+void BubbleSort(int *arr, int n);
 void SelectionSort(int *arr, int n);
 
 static void Swap(int *a, int *b);
 static void PrintArray(int *arr, int n);
+static int Compare(const void *a, const void *b);
 
 
 int main()
 {
-	int arr1[] = {12, 11, 13, 5, 6};
-	int arr2[] = {12, 11, 13, 5, 6};
-	int arr3[] = {12, 11, 13, 5, 6};
-	int n = sizeof(arr1) / sizeof(arr1[0]);
+	int arr1[SIZE], arr2[SIZE], arr3[SIZE], arr4[SIZE];
+	clock_t start;
+	clock_t end;
+	double sort_time = 0;
+	int i = 0;
+	srand(time(NULL));
+    
 
-	InsertionSort(arr1, n);
-	PrintArray(arr1, n);
+	for (i = 0; i < SIZE; ++i)
+	{
+		arr1[i] = rand();
+	}
+    
+	for (i = 0; i < SIZE; ++i)
+	{
+		arr2[i] = arr1[i];
+		arr3[i] = arr1[i];
+		arr4[i] = arr1[i];
+	}
 
-	BubbleSort(arr2, n);
-	PrintArray(arr2, n);
+
+	start = clock();
+	SelectionSort(arr1, SIZE);
+	end = clock();
+	sort_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("SelectionSort took %f seconds\n", sort_time);
 	
-	SelectionSort(arr3, n);
-	PrintArray(arr3, n);
+	
+	start = clock();
+	BubbleSort(arr2, SIZE);
+	end = clock();
+	sort_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("BubbleSort took %f seconds\n", sort_time);
+	
+	
+	start = clock();
+	InsertionSort(arr3, SIZE);
+	end = clock();
+	sort_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("InsertionSort took %f seconds\n", sort_time);
+    
+
+	start = clock();
+	qsort(arr4, SIZE, sizeof(int), Compare);
+	end = clock();
+	sort_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("qsort took %f seconds\n", sort_time);
+	
 
 	return 0;
 }
@@ -48,7 +88,7 @@ void SelectionSort(int *arr, int n)
 }
 
 
-void BubbleSort(int arr[], int n)
+void BubbleSort(int *arr, int n)
 {
 	int i = 0, j = 0;
     
@@ -81,6 +121,10 @@ void InsertionSort(int *arr, int n)
 }
 
 
+static int Compare(const void *a, const void *b)
+{
+	return (*(int *)a - *(int *)b);
+}
 
 
 static void Swap(int *a, int *b)
