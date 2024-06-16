1911fc9996cd99095e05cb1dd1c33c250b236b78
itay.lvovich <itaylvovich@gmail.com>
2024-06-16 13:39:46 +0300

    implemented Counting Sort

--- ds/src/comparison_sorts.c
+++ ds/src/comparison_sorts.c
@@ -7,10 +7,13 @@
 void InsertionSort(int *arr, int n);
 void BubbleSort(int *arr, int n);
 void SelectionSort(int *arr, int n);
+void CountingSort(int *arr, int n);
 
 static void Swap(int *a, int *b);
 static void PrintArray(int *arr, int n);
 static int Compare(const void *a, const void *b);
+static int FindMaxInArray(int *arr, int size);
+static int FindMinInArray(int *arr, int size);
 
 
 int main()
@@ -20,6 +23,10 @@ int main()
 	clock_t end;
 	double sort_time = 0;
 	int i = 0;
+	
+	int arr5[9] = {4,2,7,5,2,3,6,6,10};
+	int arr6[9] = {4,2,7,5,2,3,6,6,10};
+	
 	srand(time(NULL));
     
 
@@ -63,7 +70,19 @@ int main()
 	sort_time = ((double)(end - start)) / CLOCKS_PER_SEC;
 	printf("qsort took %f seconds\n", sort_time);
 	
-
+	
+	printf("before counting sort: \n");
+	PrintArray(arr5, 9);
+	printf("after counting sort: \n");
+	CountingSort(arr5, 9);
+	PrintArray(arr5, 9);
+	
+	printf("before qsort: \n");
+	PrintArray(arr6, 9);
+	printf("after qsort: \n");
+	qsort(arr6, 9, sizeof(int), Compare);
+	PrintArray(arr6, 9);
+	
 	return 0;
 }
 
@@ -121,6 +140,45 @@ void InsertionSort(int *arr, int n)
 }
 
 
+void CountingSort(int *arr, int n)
+{
+	int i = 0, j = 0, k = 0;
+	int max_element = FindMaxInArray(arr, n);
+	int min_element = FindMinInArray(arr, n);
+	int range_of_values = max_element - min_element;
+	int *count_arr = calloc(range_of_values + 1, sizeof(int));
+	
+	for (i = 0; i < n; ++i)
+	{
+		++count_arr[arr[i]-min_element];
+	}
+	
+	
+	/* printing the count_array for testing: */
+	/*
+	printf("Count Array: \n");
+	PrintArray(count_arr, range_of_values + 1);
+	printf("\n");
+	*/
+	
+	for (i = 0; i < range_of_values + 1; ++i)
+	{
+		int curr_number = i + min_element;
+		if (count_arr[i] != 0)
+		{
+			while (count_arr[i] > 0)
+			{
+				arr[k] = curr_number;
+				++k;
+				--count_arr[i];
+			}
+		}
+	}
+	
+	free(count_arr);
+}
+
+
 static int Compare(const void *a, const void *b)
 {
 	return (*(int *)a - *(int *)b);
@@ -145,5 +203,37 @@ static void PrintArray(int *arr, int n)
 	}
 	printf("\n");
 }
+
+
+static int FindMaxInArray(int *arr, int size)
+{
+	int i = 0;
+	int max_val = arr[i];
+	for (i = 1; i < size; ++i)
+	{
+		if (arr[i] > max_val)
+		{
+			max_val = arr[i];
+		}
+	}
+	
+	return max_val;
+}
+
+static int FindMinInArray(int *arr, int size)
+{
+	int i = 0;
+	int min_val = arr[i];
+	for (i = 1; i < size; ++i)
+	{
+		if (arr[i] < min_val)
+		{
+			min_val = arr[i];
+		}
+	}
+	
+	return min_val;
+}
+
 
 
