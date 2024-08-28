d7b57917ba3a2b13c8f67e59a6754a0ecff5ecf2
itay.lvovich <itaylvovich@gmail.com>
2024-08-28 15:03:38 +0300

    moved project to git directory

--- Java/LinkedListGen/src/src/Main.java
+++ Java/LinkedListGen/src/src/Main.java
@@ -3,6 +3,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         LinkedListGen<String> list = new LinkedListGen<>();
+        list.pushFront("five");
         list.pushFront("four");
         list.pushFront("three");
         list.pushFront("two");
