851faaf9320d8fa4675e7983b41b7d243c5b1592
itay.lvovich <itaylvovich@gmail.com>
2024-09-09 12:46:49 +0300

    test 2

--- Java/TestProject/src/Main.java
+++ Java/TestProject/src/Main.java
@@ -2,8 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("test");
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("test4");
     }
 }
