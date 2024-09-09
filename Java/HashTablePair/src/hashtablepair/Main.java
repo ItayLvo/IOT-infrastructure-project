1ac1e64b0a14a863cbc83a200df66aab3c6a0811
itay.lvovich <itaylvovich@gmail.com>
2024-09-09 12:55:07 +0300

    test seperate push

--- Java/HashTablePair/src/hashtablepair/Main.java
+++ Java/HashTablePair/src/hashtablepair/Main.java
@@ -1,7 +1,7 @@
+package hashtablepair;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("test");
-        System.out.println("test2");
     }
 }
