e19cacdfbafb61f0776b38fa46225c1976ae1557
itay.lvovich <itaylvovich@gmail.com>
2024-09-12 08:54:43 +0300

    fixed after yarden CR

--- Java/Composite/src/composite/Tree.java
+++ Java/Composite/src/composite/Tree.java
@@ -1,16 +1,27 @@
 package composite;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tree implements AbstractFile {
-    private List<AbstractFile> tree = new ArrayList<>();
+public class Tree implements PrintableFile {
+    private List<PrintableFile> tree = new ArrayList<>();
 
-    public Tree(String absolutePathOfRootDirectory) {
-        File file = new File(absolutePathOfRootDirectory);
-        tree.add(new PrimitiveFile(file));
+    public Tree(String absolutePathOfRootDirectory) throws FileNotFoundException {
+        File file;
 
+        //try to open file with given path
+        try {
+        file = new File(absolutePathOfRootDirectory); }
+        catch (NullPointerException e) {
+            throw new FileNotFoundException(absolutePathOfRootDirectory);
+        }
+
+        //add "myself" to the tree
+        tree.add(new PrimitiveFile(absolutePathOfRootDirectory));
+
+        //handle my sub-directories
         if (file.isDirectory()) {
             File[] subDirectories = file.listFiles();
             for (File subFile : subDirectories) {
@@ -18,16 +29,34 @@ public class Tree implements AbstractFile {
                     tree.add(new Tree(subFile.getAbsolutePath()));
                 }
                 else {
-                    tree.add(new PrimitiveFile(subFile));
+                    tree.add(new PrimitiveFile(subFile.getAbsolutePath()));
                 }
             }
+        }
+        //if root is not a directory
+        else {
+            throw new IllegalArgumentException();
         }
     }
 
     @Override
     public void print() {
-        for (AbstractFile file : tree) {
+        for (PrintableFile file : tree) {
             file.print();
+        }
+    }
+
+    //nested PrimitiveFile class
+    public static class PrimitiveFile implements PrintableFile {
+        private File file;
+
+        public PrimitiveFile(String path) {
+            this.file = new File(path);
+        }
+
+        @Override
+        public void print() {
+            System.out.println(file);
         }
     }
 }
